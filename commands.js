501ca9d7171b1e8534c2ddec0c9c2c960c396c65
Damon Oehlman <damon.oehlman@gmail.com>
2017-11-16 01:45:50 +1100

    use commander instead of scaffolder

---
+++ commands.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const fs = require('fs');
+const async = require('async');
+const lytroview = require('./');
+
+function split(filename, opts) {
+  const sourceFile = path.resolve(filename);
+  const outputDir = opts.output_dir || path.dirname(sourceFile);
+  const baseName = path.basename(sourceFile, path.extname(sourceFile));
+  
+  lytroview(sourceFile, (err, fileData) => {
+    if (err) {
+      return console.error(err);
+    }
+    
+    // save each of the images to the file system
+    console.log(`found ${fileData.images.length} layers`);
+    async.eachOf(fileData.images, saveJpeg, (err) => {
+      if (err) {
+        return console.error(err);
+      }
+
+      console.log('done');
+    });
+  });
+
+  function saveJpeg(image, fileIndex, callback) {
+    const targetFile = path.resolve(outputDir, `${baseName}-${fileIndex}.jpg`);
+    
+    console.log(`writing: ${targetFile}`);
+    fs.writeFile(targetFile, image.data, callback);
+  }
+}
+
+module.exports = {
+  split
+};
